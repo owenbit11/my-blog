4ef8d5dbd543e82e8cbce1fde64e2236eae9ae8c
ouii <819596262@qq.com>
2026-03-21 18:52:08 +0800

    first blog

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>我的博客</h1>
+
+      <div>
+        <h2>第一篇文章</h2>
+        <p>这是我的第一篇博客内容</p>
+      </div>
+
+      <div>
+        <h2>第二篇文章</h2>
+        <p>这是第二篇内容</p>
+      </div>
+    </div>
+  )
+}
